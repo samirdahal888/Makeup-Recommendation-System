ea6441c30bc8cfe0c8f4c4dc3b32654d71938214
samirdahal888 <samirdahal877@gmail.com>
2025-05-21 18:58:15 +0545

    normal_skin_advisor class completed

---
+++ models/normal_skin_advisor.py
@@ -0,0 +1,10 @@
+from models.base_advisor import Baseadvisor
+
+class NormalSkinAdvisor(Baseadvisor):
+    def recommend_makeup(self):
+        print("Recommended Makeup for Normal Skin:")
+        print("- Use light liquid foundation")
+        print("- You can use either cream or powder blush")
+        print("- Any lipstick type works well")
+                
+        
