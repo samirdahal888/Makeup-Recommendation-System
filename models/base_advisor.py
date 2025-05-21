739a6a071c734569932408684daadad1a0bb60a2
samirdahal888 <samirdahal877@gmail.com>
2025-05-21 18:45:11 +0545

    based_advisor class complete

---
+++ models/base_advisor.py
@@ -0,0 +1,24 @@
+from abc import ABC ,abstractmethod
+
+class Baseadvisor(ABC):
+    def __init__(self,name,face_shape,skin_tone):
+        self.__name = name
+        self._face_shape = face_shape
+        self._skin_tone = skin_tone
+
+    @abstractmethod
+    def recommend_makeup(self):
+        pass
+
+    def get_user_information(self):
+        return f" Client :{self.__name} - {self._face_shape} face ,{self._skin_tone} skin"
+    
+    @staticmethod
+    def is_valid_face_shape(shape):
+        valid_shapes = ['oval','round','square','heart']
+        return shape in valid_shapes
+    
+    @classmethod
+    def general_tips(cls):
+        return' general Tip: Always mosturie before makeup!'
+        
