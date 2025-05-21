03f08cb5e91802ef5130e0963cd20ba368ca7946
samirdahal888 <samirdahal877@gmail.com>
2025-05-21 18:54:47 +0545

    oil_skin_advisor class completed

---
+++ models/oily_skin_advisor.py
@@ -0,0 +1,9 @@
+from models.base_advisor import Baseadvisor
+
+class OilySkinAdvisor(Baseadvisor):
+    def recommend_makeup(self):
+
+        print("Recommended Makeup for Oily Skin:")
+        print("- Use matte foundation")
+        print("- Powder blush is recommended")
+        print("- Choose long-lasting lipstick")
